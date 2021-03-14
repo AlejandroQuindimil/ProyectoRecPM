e8f7d43470bf5c9f60bf1fb7513d2f5a976a02c8
alejandro.quindimil.cancio@gmail.com <alejandro.quindimil.cancio@gmail.com>
2021-03-14 11:55:16 +0100

    add: funcions Register

--- app/src/main/java/com/alejandroquindimil/proyectorecpm/RegisterActivity.java
+++ app/src/main/java/com/alejandroquindimil/proyectorecpm/RegisterActivity.java
@@ -1,14 +1,78 @@
 package com.alejandroquindimil.proyectorecpm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.Task;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText emailTextView, paswordTextView;
+    private Button Btn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        mAuth= FirebaseAuth.getInstance();
+
+        emailTextView=findViewById(R.id.act_register_editTextEmail);
+        paswordTextView= findViewById(R.id.act_register_editTextPassword);
+        Btn= findViewById(R.id.btn_register_act_register);
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+    }
+
+    private void registerNewUser() {
+
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = paswordTextView.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(), "Introduce Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(getApplicationContext(), "Introduce Contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Registro con exito", Toast.LENGTH_LONG).show();
+                            Intent intent= new Intent(RegisterActivity.this,MainActivity.class);
+                            startActivity(intent);
+
+                        }else{
+                            Toast.makeText(getApplicationContext(),
+                                    "Fallo al registrar", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        }
     }
 }
