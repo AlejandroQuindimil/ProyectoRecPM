ca48d928df35c2daa736eb17629a6b10f986d6f9
alejandro.quindimil.cancio@gmail.com <alejandro.quindimil.cancio@gmail.com>
2021-03-16 18:19:42 +0100

    Add campos para recogerlos en el sharedpreferences

--- app/src/main/java/com/alejandroquindimil/proyectorecpm/login/RegisterActivity.java
+++ app/src/main/java/com/alejandroquindimil/proyectorecpm/login/RegisterActivity.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -16,20 +16,17 @@ import android.widget.Toast;
 import com.alejandroquindimil.proyectorecpm.MainActivity;
 import com.alejandroquindimil.proyectorecpm.R;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.AuthResult;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.FirebaseUserMetadata;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText emailTextView, paswordTextView, repeatpaswordTextView;
+    private EditText userName, emailTextView, paswordTextView, repeatpassextView;
     private Button BtnRegister,BtnLogin;
     private ProgressBar progressbar;
     private FirebaseAuth mAuth;
-    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +36,15 @@ public class RegisterActivity extends AppCompatActivity {
 
         mAuth= FirebaseAuth.getInstance();
 
+        userName=findViewById(R.id.act_register_editTest_user);
         emailTextView=findViewById(R.id.act_register_editTextEmail);
         paswordTextView= findViewById(R.id.act_register_editTextPassword);
-        repeatpaswordTextView= findViewById(R.id.act_register_editTextrepeatPass);
+        repeatpassextView= findViewById(R.id.act_register_editTextrepeatPass);
+
         BtnRegister= findViewById(R.id.btn_register_act_register);
         BtnLogin= findViewById(R.id.btn_login_act_register);
         progressbar = findViewById(R.id.progressbar);
+
 
         BtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,11 +70,19 @@ public class RegisterActivity extends AppCompatActivity {
     private void registerNewUser() {
         progressbar.setVisibility(View.VISIBLE);
 
-        String email, password, confirm;
+        String user,email, password, confirm;
+        user= userName.getText().toString();
         email = emailTextView.getText().toString();
         password = paswordTextView.getText().toString();
-        confirm=repeatpaswordTextView.getText().toString();
+        confirm=repeatpassextView.getText().toString();
         progressbar = findViewById(R.id.progressbar);
+
+
+        if (TextUtils.isEmpty(user)){
+            Toast.makeText(getApplicationContext(), "Introduce nombre", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         if (TextUtils.isEmpty(email)){
             Toast.makeText(getApplicationContext(), "Introduce Email", Toast.LENGTH_SHORT).show();
             return;
@@ -89,18 +97,12 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
+
         mAuth.createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
-                            currentUser= mAuth.getCurrentUser();
-                            currentUser = mAuth.getCurrentUser();
-                            String disp = currentUser.getDisplayName();
-                            String email = currentUser.getEmail();
-                            FirebaseUserMetadata meta = currentUser.getMetadata();
-                            Uri photo = currentUser.getPhotoUrl();
-
 
                             Toast.makeText(getApplicationContext(),
                                     "Registro con exito", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/alejandroquindimil/proyectorecpm/main/UserFragment.java
+++ app/src/main/java/com/alejandroquindimil/proyectorecpm/main/UserFragment.java
@@ -1,6 +1,7 @@
 package com.alejandroquindimil.proyectorecpm.main;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,20 +14,22 @@ import androidx.fragment.app.Fragment;
 import com.alejandroquindimil.proyectorecpm.R;
 import com.alejandroquindimil.proyectorecpm.login.RegisterActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class UserFragment extends Fragment {
     
     private Button btnLogout,btnEditar;
-    private EditText Usuario, Userrname, Email;
+    private EditText Usuario, Username, Email;
 
     private FirebaseAuth mAuth;
-    private FirebaseUser currentUser;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mAuth= FirebaseAuth.getInstance();
+
+
         View v = inflater.inflate(R.layout.fragment_user, container, false);
 
         initViews(v);
@@ -66,8 +69,10 @@ public class UserFragment extends Fragment {
         btnEditar =v.findViewById(R.id.frg_user_btn_edit);
 
         Usuario= v.findViewById(R.id.frg_user_tView_userName);
-        Userrname = v.findViewById(R.id.frg_user_name_tviem);
+        Username = v.findViewById(R.id.frg_user_name_tviem);
         Email = v.findViewById(R.id.frg_user_email_tView);
+
+
 
     }
 }
